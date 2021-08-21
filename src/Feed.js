8909fdecb0a56576192fc7bcb72b309eb2dd7b71
Pinky057 <ishratjahanpinky2@gmail.com>
2021-08-21 21:02:59 +0600

    linedin clone body header

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/Event';
+
 import './Feed.css';
+import InputOptions from './InputOptions';
 
 
 function Feed() {
@@ -13,6 +19,12 @@ function Feed() {
                         <input type="text" />
                         <button type="submit" >Send</button>
                     </form>
+                </div>
+                <div className="feed_inputOptions">
+                 <InputOptions Icon={ImageIcon}  title='Photo' color='#70B5F9'/>
+                 <InputOptions Icon={SubscriptionsIcon}  title='Video' color='#E7A33E'/>
+                 <InputOptions Icon={EventNoteIcon}  title='Event' color='#C0CBCD'/>
+                 <InputOptions Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E'/>
                 </div>
             </div>
         </div>
